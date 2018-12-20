41828035742acb5ce0fee4e0e09b6045e74e2140
Tang <eytang8@gmail.com>
2018-12-20 00:00:48 -0800

    created header component

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+{/*
+this is gonna be the header. it will have a link back to the landing page (my name) and a menu that will generate a few links to various pages
+*/}
+
+class Header extends Component {
+	constructor(props){
+		// props should include home + array for img/txt/nfo/etc
+		// navNames / navUrls
+		super(props);
+	}
+
+	render(){
+		const navButtons = [];
+		for (let i = 0; i < this.props.navNames.length; i++){
+			navButtons.push(
+				<a href={this.props.navUrls[i]}>
+					{this.props.navNames[i]}
+				</a>
+			)
+		}
+		return (
+			<div>
+				<a href={this.props.home.url}>{this.props.home.txt}</a>
+				{navButtons}
+			</div>
+		)
+	
+	}
+}
+
+export default Header;
